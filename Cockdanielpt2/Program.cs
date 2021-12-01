a59bc72583e080604d84d92fa754eefaf1b9ed63
Tali04001 <89461076+Tali04001@users.noreply.github.com>
2021-12-01 11:49:07 +0100

    Add files via upload

---
+++ Cockdanielpt2/Program.cs
@@ -0,0 +1,62 @@
+﻿using System;
+
+namespace Cockdanielpt2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /* //Cylinder area.
+            Console.WriteLine("skriv in radie");
+            int r = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("skriv in höjd");
+            int h = int.Parse(Console.ReadLine());
+            
+            Console.WriteLine(r * r * Math.PI * h); */
+
+            /*//Några if satser lmao
+
+            Console.WriteLine("Ålder? för glass:");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if(ålder <= 10){
+                Console.WriteLine("Gratis glass");
+            }
+            else if(ålder > 10){
+                Console.WriteLine("20 kr bitch >:)");
+            } */
+
+            /*//köpalkohol 
+            Console.WriteLine("HUR GAMMAL???");
+            int ålder = int.Parse(Console.ReadLine());
+
+            if(ålder >=20){
+                Console.WriteLine("köp alkohol!");
+            }
+
+            if(ålder > 80){ // Får fortfarande köpa alkohol men borde inte
+                Console.WriteLine("Din gammla håsna lägg dig i graven.");
+            }
+
+            else if(ålder < 20){
+                Console.WriteLine("Håll käften bäbis och drick mjölk!");
+            } */
+
+           /* //summa av 201 tal adderade med varandra.
+
+            int s = 0;
+            int g = 0;
+
+            do {
+                s+=g;
+                g++;
+            } while(g<=201);
+
+            Console.WriteLine(s); */
+
+
+
+        }
+    }
+}
